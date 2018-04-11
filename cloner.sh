536b875cc4e5bbd6cab8dab843d1e4329e87f99b
sandyorton7 <sandyorton7@gmail.com>
2018-04-11 12:08:30 +0000

    initial commit

---
+++ cloner.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+git clone https://github.com/boulzordev/android_device_motorola_potter -b lineage-15.1-64  device/motorola/potter
+git clone https://github.com/boulzordev/android_vendor_motorola_potter -b lineage-15.1-64 vendor/motorola/potter
+git clone https://github.com/boulzordev/android_kernel_motorola_msm8953 -b lineage-15.1-64  kernel/motorola/msm8953
+git clone https://github.com/lineageos/android_external_bson -b lineage-15.1 external/bson
+git clone https://github.com/lineageos/android_packages_resources_devicesettings -b lineage-15.1 packages/resources/devicesettings
+git clone https://github.com/boulzordev/android_hardware_qcom_display.git -b bzr-8.1-8937 hardware/qcom/display-caf/msm8937
+git clone https://github.com/boulzordev/android_hardware_qcom_audio.git -b bzr-8.1-8937 hardware/qcom/audio-caf/msm8937
+git clone https://github.com/boulzordev/android_hardware_qcom_media.git -b bzr-8.1-8937 hardware/qcom/media-caf/msm8937
+
